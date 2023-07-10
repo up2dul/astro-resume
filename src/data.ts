b16dcb723e8a11c3d2722aaa78d859b16e006631
abdul <malik301002@gmail.com>
2023-07-11 00:13:11 +0700

    feat: create data file

---
+++ src/data.ts
@@ -0,0 +1,70 @@
+export const data = {
+  contacts: [
+    {
+      url: 'mailto:malik301002@gmail.com',
+      display: 'malik301002@gmail.com',
+    },
+    {
+      url: 'https://up2dul.com',
+      display: 'up2dul.com',
+    },
+    {
+      url: 'https://github.com/up2dul',
+      display: 'github.com/up2dul',
+    },
+    {
+      url: 'https://linkedin.com/in/abdulmalikk',
+      display: 'linkedin.com/in/abdulmalikk',
+    },
+  ],
+  experiences: [
+    {
+      title: 'Frontend Developer',
+      type: 'Full-time',
+      period: 'May 2023 - Present',
+      company: 'Svara',
+      companyUrl: 'https://svara.id',
+      location: 'Bandung',
+      responsibilities: ['-'],
+    },
+    {
+      title: 'External Code Reviewer',
+      type: 'Part-time',
+      period: 'April 2023 - Present',
+      company: 'Dicoding',
+      companyUrl: 'https://dicoding.com',
+      location: 'Remote',
+      responsibilities: [
+        "Reviewing student's submissions (assignments) in a course according to the given target",
+        "Provide solution and suggestion to student's submission",
+        'Help to answer the forum discussion',
+      ],
+    },
+  ],
+  educations: [
+    {
+      title: 'SMK Media Informatika',
+      type: 'High School Diploma',
+      degree: 'Computer Software Engineering',
+      period: 'July 2017 - May 2020',
+      url: 'https://smkmediainformatika.sch.id',
+      location: 'Jakarta',
+    },
+  ],
+  skills: [
+    ['Figma', 'Git', 'GitHub', 'GitLab'],
+    ['Markdown', 'HTML', 'CSS', 'SCSS', 'JavaScript', 'TypeScript', 'Node.js'],
+    [
+      'React.js',
+      'Next.js',
+      'Astro',
+      'Redux',
+      'Zustand',
+      'Tailwind',
+      'Chakra UI',
+      'Mantine',
+      'Headless UI',
+      'Radix',
+    ],
+  ],
+};
